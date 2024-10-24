119d7b7eceb5392dcfa4229c5b6b39dc3fa4d17c
Marcin Wawer <marcinwaw_git@icloud.com>
2024-10-24 15:56:30 +0200

    [init] repo for UniveristyOrganizer app - project for 'mobile apps for the ios' course

---
+++ UniversityOrganizer/UniversityOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversityOrganizerApp.swift
+//  UniversityOrganizer
+//
+//  Created by Marcin Wawer on 24-10-2024.
+//
+
+import SwiftUI
+
+@main
+struct UniversityOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
