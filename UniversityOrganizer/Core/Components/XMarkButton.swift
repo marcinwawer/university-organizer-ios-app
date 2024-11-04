9e1b743ce93777db0986bae12d3a78c566755455
Marcin Wawer <marcinwaw_git@icloud.com>
2024-11-04 18:35:09 +0100

    [feature] add xmark button component

---
+++ UniversityOrganizer/Core/Components/XMarkButton.swift
@@ -0,0 +1,21 @@
+//
+//  XMarkButton.swift
+//  UniversityOrganizer
+//
+//  Created by Marcin Wawer on 04/11/2024.
+//
+
+import SwiftUI
+
+import SwiftUI
+
+struct XMarkButton: View {
+    var body: some View {
+        Image(systemName: "xmark")
+            .font(.headline)
+    }
+}
+
+#Preview {
+    XMarkButton()
+}
