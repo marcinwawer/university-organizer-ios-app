89948867edd8c17f02c39017d2c14e570ac40a27
Marcin Wawer <marcinwaw_git@icloud.com>
2024-11-21 11:59:27 +0100

    [feature] connect parser with swiftdata

--- UniversityOrganizer/Services/ICSParser.swift
+++ UniversityOrganizer/Services/ICSParser.swift
@@ -6,64 +6,84 @@
 //
 
 import SwiftUI
-import Foundation
+import SwiftData
 
-// Funkcja do odczytu pliku `.ics`
-func readICSFile(named fileName: String) {
-    if let path = Bundle.main.path(forResource: fileName, ofType: "ics") {
-        do {
-            let fileContent = try String(contentsOfFile: path, encoding: .utf8)
-            parseICSContent(fileContent)
-        } catch {
-            print("Error while reading the file: \(error)")
-        }
-    } else {
-        print("\(fileName).ics was not found.")
-    }
-}
-
-func parseICSContent(_ content: String) {
-    var scheduleMap: [String: [[String]]] = [:]
+func parseICSContent(_ content: String, context: ModelContext) {
     let events = content.components(separatedBy: "BEGIN:VEVENT")
     
     for event in events {
         if let summary = extractField(from: event, fieldName: "SUMMARY"),
            let dtstart = extractField(from: event, fieldName: "DTSTART;VALUE=DATE-TIME"),
            let dtend = extractField(from: event, fieldName: "DTEND;VALUE=DATE-TIME"),
+           let description = extractField(from: event, fieldName: "DESCRIPTION"),
            let startTime = extractTime(from: dtstart),
-           let endTime = extractTime(from: dtend) {
+           let endTime = extractTime(from: dtend),
+           let dayOfTheWeek = extractDayOfWeek(from: dtstart) {
             
-            let timePair = [startTime, endTime]
+            let firstChar = summary.first
+            guard let subjectType = SubjectType.from(character: firstChar ?? " ") else {
+                continue
+            }
+            let cleanedSummary = summary.dropFirst(3).trimmingCharacters(in: .whitespacesAndNewlines)
+            let room = extractRoom(from: description)
+            let building = extractBuilding(from: description)
             
-            if var timesArray = scheduleMap[summary] {
-                if !timesArray.contains(timePair) {
-                    timesArray.append(timePair)
-                    scheduleMap[summary] = timesArray
+            var subject: Subject? = try? context.fetch(
+                FetchDescriptor(
+                    predicate: #Predicate { subject in
+                        subject.name == cleanedSummary &&
+                        subject.type == subjectType.rawValue
+                    }
+                )
+            ).first
+            
+            if subject == nil {
+                subject = Subject(name: cleanedSummary, type: subjectType, room: room, building: building)
+                context.insert(subject!)
+            }
+            
+            if let subject = subject {
+                if !subject.schedules.contains(where: { $0.startTime == startTime && $0.endTime == endTime }) {
+                    let schedule = Schedule(startTime: startTime, endTime: endTime, dayOfTheWeek: dayOfTheWeek)
+                    subject.schedules.append(schedule)
                 }
-            } else {
-                scheduleMap[summary] = [timePair]
             }
         }
     }
     
-    // Wyświetlanie wyników dla każdego przedmiotu
-    for (summary, times) in scheduleMap {
-        print("Nazwa zajęć: \(summary)")
-        for time in times {
-            print("Czas rozpoczęcia: \(time[0]), Czas zakończenia: \(time[1])")
-        }
-        print("---------------")
+    do {
+        try context.save()
+        print("Dane zostały zapisane w SwiftData.")
+    } catch {
+        print("Błąd zapisu danych: \(error)")
     }
 }
 
-// Funkcja pomocnicza do wyciągania danych z danego pola
+func extractRoom(from description: String) -> String? {
+    if let roomRange = description.range(of: "Sala: ") {
+        let start = description[roomRange.upperBound...]
+        if let room = start.components(separatedBy: "\\").first?.trimmingCharacters(in: .whitespacesAndNewlines) {
+            return room
+        }
+    }
+    return nil
+}
+
+func extractBuilding(from description: String) -> String? {
+    if let startRange = description.range(of: "["),
+       let endRange = description.range(of: "]", range: startRange.upperBound..<description.endIndex) {
+        let building = description[startRange.upperBound..<endRange.lowerBound]
+        return String(building).trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+    return nil
+}
+
 func extractField(from text: String, fieldName: String) -> String? {
     guard let range = text.range(of: "\(fieldName):") else { return nil }
     let start = text[range.upperBound...]
     return start.components(separatedBy: "\n").first?.trimmingCharacters(in: .whitespacesAndNewlines)
 }
 
-// Funkcja pomocnicza do wyciągania czasu (HH:MM) z pola `DTSTART` lub `DTEND`
 func extractTime(from dateTime: String) -> String? {
     guard dateTime.count >= 13 else { return nil }
     let hour = dateTime[dateTime.index(dateTime.startIndex, offsetBy: 9)..<dateTime.index(dateTime.startIndex, offsetBy: 11)]
@@ -71,5 +91,18 @@ func extractTime(from dateTime: String) -> String? {
     return "\(hour):\(minute)"
 }
 
-
-
+func extractDayOfWeek(from dateString: String) -> Int? {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "yyyyMMdd'T'HHmmss"
+    formatter.timeZone = TimeZone(secondsFromGMT: 0)
+    
+    guard let date = formatter.date(from: dateString) else {
+        return nil
+    }
+    
+    var calendar = Calendar.current
+    calendar.firstWeekday = 2
+    
+    let weekdayNumber = calendar.component(.weekday, from: date)
+    return (weekdayNumber - calendar.firstWeekday + 7) % 7
+}
--- UniversityOrganizer/UniversityOrganizerApp.swift
+++ UniversityOrganizer/UniversityOrganizerApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct UniversityOrganizerApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: Subject.self)
         }
     }
 }
